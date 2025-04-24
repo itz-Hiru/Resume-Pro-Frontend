e4c64d040c00846de73903bc3ac7eb2d3ef31bd3
itz-Hiru <hirumithagithub@gmail.com>
2025-04-24 18:45:33 +0530

    Added required libraries and create template

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Toaster } from "react-hot-toast";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Dashboard from "./pages/Home/Dashboard.page";
+import LandingPage from "./pages/LandingPage.page";
+import EditResume from "./pages/ResumeUpdate/EditResume.page";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <div>
+        <Router>
+          <Routes>
+            {/* Default Route */}
+            <Route path="/" element={<LandingPage />} />
 
-export default App
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/resume/:resumeId" element={<EditResume />} />
+          </Routes>
+        </Router>
+      </div>
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            fontSize: "13px",
+          },
+        }}
+      />
+    </>
+  );
+};
+
+export default App;
