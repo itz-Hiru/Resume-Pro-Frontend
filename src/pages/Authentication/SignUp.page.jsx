6cc878421921a82aac644df179bdda7cbb705920
itz-Hiru <hirumithagithub@gmail.com>
2025-04-25 12:26:55 +0530

    Created signup page

--- src/pages/Authentication/SignUp.page.jsx
+++ src/pages/Authentication/SignUp.page.jsx
@@ -1,9 +1,108 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Input from "../../components/Inputs/Input.component";
+import { validateEmail, validatePassword } from "../../utils/helper.util";
+import ProfilePhotoSelector from "../../components/Inputs/ProfilePhotoSelector.component";
 
-const SignUp = () => {
+const SignUp = ({ setCurrentPage }) => {
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    let profileImageUrl = "";
+
+    if (!fullName) {
+      setError("Oops! We need your name to get started.");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError("Hmm... that doesn’t look like a valid email. Try again?");
+      return;
+    }
+
+    if (!validatePassword(password)) {
+      setError(
+        "Your password needs a bit more power: use 8+ characters, including uppercase, lowercase, a number, and a special character."
+      );
+      return;
+    }
+
+    setError("");
+
+    try {
+    } catch (e) {
+      setError(
+        "Something went wrong. Please double-check your info and try again."
+      );
+    }
+  };
+
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center">
+      <h3 className="text-2xl font-bold text-black">
+        Let's Create Something Amazing!
+      </h3>
+      <p className="text-sm text-slate-700 mt-[5px] mb-6">
+        Fill in your details and get ready to build a resume that stands out.
+      </p>
 
-export default SignUp
+      <form onSubmit={handleSignup}>
+        <ProfilePhotoSelector 
+          image={profilePic}
+          setImage={setProfilePic}
+        />
+        <div className="grid grid-cols-1 md:grid-cols-1 gap-2">
+          <Input
+            value={fullName}
+            onChange={({ target }) => setFullName(target.value)}
+            label="Full Name"
+            placeholder="e.g. Alex Morgan"
+            type="text"
+          />
+          <Input
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="e.g. alex@domain.com"
+            type="text"
+          />
+          <Input
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            label="Create a Strong Password"
+            placeholder="Use a strong one"
+            type="password"
+          />
+        </div>
+        {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+        <button
+          type="submit"
+          className="btn-primary bg-primary text-white py-2 rounded-md hover:bg-primary-dark transition duration-300"
+        >
+          Create My Account
+        </button>
+
+        <p className="text-sm text-slate-800 mt-3">
+          Already have an account?{" "}
+          <button
+            className="font-semibold text-primary underline cursor-pointer"
+            onClick={() => setCurrentPage("login")}
+          >
+            Login
+          </button>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
--- src/utils/helper.util.js
+++ src/utils/helper.util.js
@@ -2,3 +2,8 @@ export const validateEmail = (email) => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
 }
+
+export const validatePassword = (password) => {
+    const regex =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?#&_])[A-Za-z\d@$!%*?#&_]{8,}$/;
+    return regex.test(password);
+}
