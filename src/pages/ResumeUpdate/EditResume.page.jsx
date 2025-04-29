5a203c5b049d5d7f3cca50d1d09956782f1cecf3
itz-Hiru <hirumithagithub@gmail.com>
2025-04-29 11:27:53 +0530

    Created edit resume title

--- src/pages/ResumeUpdate/EditResume.page.jsx
+++ src/pages/ResumeUpdate/EditResume.page.jsx
@@ -1,9 +1,227 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useReactToPrint } from "react-to-print";
+import {
+  LuArrowLeft,
+  LuCircleAlert,
+  LuDownload,
+  LuPalette,
+  LuSave,
+  LuTrash2,
+} from "react-icons/lu";
+import toast from "react-hot-toast";
+import DashboardLayout from "../../components/Layouts/DashboardLayout.component";
+import TitleInput from "../../components/Inputs/TitleInput.component";
+import axiosInstance from "../../utils/axiosInstance.util";
+import { API_PATHS } from "../../utils/apiPath.util";
 
 const EditResume = () => {
-  return (
-    <div>EditResume</div>
-  )
-}
+  const { resumeId } = useParams();
+  const navigate = useNavigate();
 
-export default EditResume
+  const resumeRef = useRef(null);
+  const resumeDownloadRef = useRef(null);
+
+  const [baseWidth, setBaseWidth] = useState(800);
+  const [openThemeSelector, setOpenThemeSelector] = useState(false);
+  const [openPreviewModal, setOpenPreviewModal] = useState(false);
+  const [currentPage, setCurrentPage] = useState("profile-info");
+  const [progress, setProgress] = useState(0);
+  const [resumeData, setResumeData] = useState({
+    title: "",
+    thumbnailLink: "",
+    profileInfo: {
+      profileImg: null,
+      profilePreviewUrl: "",
+      fullName: "",
+      designation: "",
+      summary: "",
+    },
+    template: {
+      theme: "",
+      colorPalette: "",
+    },
+    contactInfo: {
+      email: "",
+      phone: "",
+      location: "",
+      linkedin: "",
+      github: "",
+      website: "",
+    },
+    workExperience: [
+      {
+        company: "",
+        role: "",
+        startDate: "", // eg: "2025-04"
+        endDate: "", // eg: "2025-12"
+        description: "",
+      },
+    ],
+    education: [
+      {
+        degree: "",
+        institution: "",
+        startDate: "",
+        endDate: "",
+      },
+    ],
+    skills: [
+      {
+        name: "",
+        progress: 0, // Precentage value (0-100)
+      },
+    ],
+    projects: [
+      {
+        name: "",
+        description: "",
+        github: "",
+        liveDemo: "",
+      },
+    ],
+    certifications: [
+      {
+        title: "",
+        issuer: "",
+        year: "",
+      },
+    ],
+    languages: [
+      {
+        name: "",
+        progress: 0, // Precentage value (0-100)
+      },
+    ],
+    interests: [""],
+  });
+  const [errorMsg, setErrorMsg] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Validate Inputs
+  const validateAndNext = (e) => {};
+
+  // Function to navigate to next page
+  const goToNextPage = () => {};
+
+  // Function to navigate to previous page
+  const goToPrevPage = () => {};
+
+  const renderForm = () => {};
+
+  // Update simple nested objects (like profile info, contact etc.)
+  const updateSection = (section, key, value) => {};
+
+  // Update array items (like work experience, skills etc.)
+  const updateArrayItem = (section, index, key, value) => {};
+
+  // Add items to array
+  const addArrayItem = (section, newItem) => {};
+
+  // Remove item from array
+  const removeArrayItem = (section, index) => {};
+
+  // Fetch resume info by ID
+  const fetchResumeDetailsById = async () => {
+    try {
+      const response = await axiosInstance.get(
+        API_PATHS.RESUME.GET_RESUME_BY_ID(resumeId)
+      );
+
+      const resumeInfo = response.data.resume;
+
+      if (resumeInfo && resumeInfo.profileInfo) {
+        setResumeData((prevState) => ({
+          ...prevState,
+          title: resumeInfo?.title || "Untitled",
+          template: resumeInfo?.template || prevState?.template,
+          profileInfo: resumeInfo?.profileInfo || prevState?.profileInfo,
+          contactInfo: resumeInfo?.contactInfo || prevState?.contactInfo,
+          workExperience:
+            resumeInfo?.workExperience || prevState?.workExperience,
+          education: resumeInfo?.education || prevState?.education,
+          skills: resumeInfo?.skills || prevState?.skills,
+          projects: resumeInfo?.projects || prevState?.projects,
+          certifications:
+            resumeInfo?.certifications || prevState?.certifications,
+          languages: resumeInfo?.languages || prevState?.languages,
+          interests: resumeInfo?.interests || prevState?.interests,
+        }));
+      }
+    } catch (error) {
+      console.error("Error fetching resume data: ", error);
+    }
+  };
+
+  // Upload thumbnail and resume profile image
+  const uploadResumeImage = async () => {};
+
+  const updateResumeDetails = async (thumbnailLink, getProfilePreviewUrl) => {};
+
+  // Delete resume
+  const handleDeleteResume = async () => {};
+
+  // Download resume
+  const reactToPrintFn = useReactToPrint({ contentRef: resumeDownloadRef });
+
+  // Update resume basewidth based on resume container size
+  const updateBaseWidth = () => {};
+
+  useEffect(() => {
+    updateBaseWidth();
+    window.addEventListener("resize", updateBaseWidth);
+
+    if (resumeId) {
+      fetchResumeDetailsById();
+    }
+
+    return () => {
+      window.removeEventListener("resize", updateBaseWidth);
+    };
+  }, []);
+  return (
+    <DashboardLayout>
+      <div className="conatiner mx-auto">
+        <div className="flex items-center justify-between gap-5 bg-white rounded-lg border border-purple-100 py-3 px-4 mb-4">
+          <TitleInput
+            title={resumeData.title}
+            setTitle={(value) =>
+              setResumeData((prevState) => ({
+                ...prevState,
+                title: value,
+              }))
+            }
+          />
+          <div className="flex items-center gap-4">
+            <button
+              className="btn-small-light"
+              onClick={() => setOpenThemeSelector(true)}
+            >
+              <LuPalette className="text-[16px]" />
+              <span className="hidden md:block">Change Theme</span>
+            </button>
+            <button
+              className="btn-small-light-delete"
+              onClick={handleDeleteResume}
+            >
+              <LuTrash2 className="text-[16px]" />
+              <span className="hidden md:block">Delete</span>
+            </button>
+            <button
+              className="btn-small-light-download"
+              onClick={() => setOpenPreviewModal(true)}
+            >
+              <LuDownload className="text-[16px]" />
+              <span className="hidden md:block">Preview & Download</span>
+            </button>
+          </div>
+        </div>
+        <div className="">
+          <div className=""></div>
+        </div>
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default EditResume;
